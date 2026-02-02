8c254fd4ecc55a6bbf3ca93d505dd936ddcff884
nitesh172 <niteshbaghel172@gmail.com>
2026-02-02 21:28:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react"
+import "./App.css"
+import CountryCard from "./components/country-card"
+
+function App() {
+  const [countries, setCountries] = useState([])
+
+  const fetchCountries = async () => {
+    try {
+      const response = await fetch(
+        "https://xcountries-backend.labs.crio.do/all",
+      )
+      const data = await response.json()
+      setCountries(data)
+    } catch (error) {
+      console.error("Error fetching countries:", error)
+    }
+  }
+
+  useEffect(() => {
+    fetchCountries()
+  }, [])
+
+  return (
+    <div className="countries-list">
+      {countries.map((country) => (
+        <CountryCard key={country.abbr} country={country} />
+      ))}
+    </div>
+  )
+}
+
+export default App
